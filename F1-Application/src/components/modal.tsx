cb089a5b89e257f2e7e384442939b48f40640b11
Matthew Lindstrom <77463748+mlind423@users.noreply.github.com>
2024-03-22 13:39:09 -0600

    Added the main modal
    The main modal will get the information from the element it is generated from and dynamically change its contents based on the information provided.

---
+++ F1-Application/src/components/modal.tsx
@@ -0,0 +1,19 @@
+export default function Modal(props) {
+    return(
+        <div>
+            <button className="btn" onClick={() => document.getElementById('my_modal_2').showModal()}>open modal</button>
+            <dialog id="my_modal_2" className="modal">
+                <div className="modal-box">
+                    <form method="dialog">
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">Press ESC key or click outside to close</p>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
+        </div>
+    )
+}
