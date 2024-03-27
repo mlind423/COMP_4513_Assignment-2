7df5e4466fdd3e31c420070df98d6e28d0bcad01
mlind423 <mlind423@mtroyal.ca>
2024-03-27 15:26:12 -0600

    Added a favorites button and cleaned up layout

--- F1-Application/src/components/modalComponents/driver.tsx
+++ F1-Application/src/components/modalComponents/driver.tsx
@@ -1,12 +1,31 @@
 import { useState } from "react"
 export default function DriverContent(props) {
+    // I Should move this to a different file since it will need to be used on multiple modals
+    const [fav, setFav] = useState('♡')
+    const handler = () => {
+        if(fav == '♡'){
+            setFav("♥")
+        }else{
+            setFav("♡")
+        }
+    }
     if (props.type == "driver") {
         return (
-            <div className="flex flex-col w-full modal-box w-100">
-                <div className="grid h-20 card bg-base-300 rounded-box place-items-center">
+            <div className="flex flex-col w-full modal-box w-100 px-3 py-3">
                     <h1>Driver details</h1>
-                    <p>{props.forename} {props.surname}</p>
-                </div>
+                    <div className="flex w-full">
+                        <div className="grid h-auto flex-grow card bg-base-300 rounded-box grid grid-cols-2 gap-4 place-items-center">
+                            <div className="gird grid-rows-5 gap-4 px-3 py-3">
+                                <p>{props.forename} {props.surname}</p>
+                                <p>{props.forename} {props.surname}</p>
+                                <p>{props.forename} {props.surname}</p>
+                                <p>{props.forename} {props.surname}</p>
+                                <p>{props.forename} {props.surname}</p>
+                            </div> 
+                            <button className="btn text-3xl text-green-500 btn-circle" onClick={handler}>{fav}</button>
+                        </div>
+                        
+                    </div>
                 <div className="divider"></div>
                 <div className="grid h-max card bg-base-300 rounded-box place-items-center">
                     <img className="object-contain h-max max-w-xs " src="https://images.pexels.com/photos/17491423/pexels-photo-17491423/free-photo-of-f1-mclaren-mp4-9.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
--- F1-Application/src/components/modalComponents/modal.tsx
+++ F1-Application/src/components/modalComponents/modal.tsx
@@ -5,7 +5,7 @@ export default function Modal(props) {
         <div>
             <button className="btn btn-sm rounded-full bg-inherit border-transparent text-inherit border-0" onClick={() => document.getElementById(`${props.refId}`).showModal()}>{props.text}</button>
             <dialog id={props.refId} className="modal w-auto">
-                <div className="modal-box px-10 py-10 w-4/5">
+                <div className="modal-box px-10 py-10 w-4/5 overflow-hidden">
                     <form method="dialog">
                         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     </form>
