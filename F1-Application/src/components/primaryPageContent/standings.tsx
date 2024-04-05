bff2a7824023d3ad03202a00f7335a6ad410d3df
Matthew Lindstrom <77463748+mlind423@users.noreply.github.com>
2024-04-05 14:22:31 -0600

    Update standings.tsx

--- F1-Application/src/components/primaryPageContent/standings.tsx
+++ F1-Application/src/components/primaryPageContent/standings.tsx
@@ -1,4 +1,5 @@
 import Modal from "../modalComponents/modal";
+import { useState } from "react";
 export default function PrimaryPageContent(props:any) {
     if (!(props.data.drivers && props.data.constructors)){
         return <span className="loading loading-spinner loading-lg"></span>;
