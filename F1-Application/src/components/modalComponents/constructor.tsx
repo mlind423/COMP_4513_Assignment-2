448a8c6b71af1f78375e3713d0a50b9fba766fc4
Matthew Lindstrom <77463748+mlind423@users.noreply.github.com>
2024-04-06 15:07:18 -0600

    Update constructor.tsx

--- F1-Application/src/components/modalComponents/constructor.tsx
+++ F1-Application/src/components/modalComponents/constructor.tsx
@@ -24,6 +24,7 @@ export default function ConstructorContent(props: any) {
                         </div>
                     </div>
                 </div>
+                <div className="divider"></div>
                 <div className="grid size-auto card bg-base-300 rounded-box place-items-center">
                     <ImgRequest className="object-contain max-w-md m-4" src={source} srcDefault="https://placehold.co/600x400"></ImgRequest>
                 </div>
