76516c81f2919173e12c6a8266047e600b90f323
Matthew Lindstrom <77463748+mlind423@users.noreply.github.com>
2024-04-07 10:12:36 -0600

    Update favourites.tsx

--- F1-Application/src/components/modalComponents/favourites.tsx
+++ F1-Application/src/components/modalComponents/favourites.tsx
@@ -12,8 +12,8 @@ export default function FavouriteContent(props: any) {
             localStorage.setItem('drivers', '')
         }else if(e.target.id === 'con'){
             localStorage.setItem('constructors', '')
-        }else if(e.target.id === 'circuit'){
-            localStorage.setItem('cir', "")
+        }else if(e.target.id === 'cir'){
+            localStorage.setItem('circuits', "")
         }
         setRem([])
     }
@@ -24,9 +24,9 @@ export default function FavouriteContent(props: any) {
             <div className="flex flex-col modal-box max-w-none w-full px-3 py-3">
                 <h2 className="text-xl">Favorites</h2>
                 <div className="flex flex-col lg:flex-row">
-                    <div className="text-nowrap overflow-auto grid flex-grow w-auto max-h-60 card bg-base-300 rounded-box place-items-center px-3 py-3 place-content-start">
+                    <div className="text-nowrap overflow-auto grid flex-grow w-auto max-h-60 card bg-base-300 rounded-box px-3 py-3 place-items-center">
                     <h2 className="text-lg">Drivers</h2> <button id='dr' onClick={handleRemove} className="text-secondary-content btn btn-secondary btn-sm rounded-full m-2 border-transparent text-secondary-content border-0  hover:btn-primary hover:text-primary-content"> Remove All </button>
-                        <ul>
+                        <ul className="place-self-start">
                             {driver.map((e: any, index: number) => {
                                 return <li key={index}>{e}</li>
                             })}
@@ -34,9 +34,9 @@ export default function FavouriteContent(props: any) {
                     </div>
                     <div className="divider lg:divider-horizontal">
                     </div>
-                    <div className="text-nowrap w-auto max-h-60 grid flex-grow card bg-base-300 rounded-box place-items-center px-3 py-3 place-content-start">
+                    <div className="text-nowrap w-auto max-h-60 grid flex-grow card bg-base-300 rounded-box px-3 py-3 place-items-center ">
                         <h2 className="text-lg">Constuctors</h2><button id='con' onClick={handleRemove} className="text-secondary-content btn btn-secondary btn-sm rounded-full m-2 border-transparent text-secondary-content border-0  hover:btn-primary hover:text-primary-content"> Remove All </button>
-                        <ul>
+                        <ul className="place-self-start">
                             {constructor.map((e: any, index: number) => {
                                 return <li key={index}>{e}</li>
                             })}
@@ -44,9 +44,9 @@ export default function FavouriteContent(props: any) {
                     </div>
                     <div className="divider lg:divider-horizontal">
                     </div>
-                    <div className="text-nowrap w-auto max-h-60 grid flex-grow card bg-base-300 rounded-box place-items-center px-3 py-3 place-content-start">
+                    <div className="text-nowrap w-auto max-h-60 grid flex-grow card bg-base-300 rounded-box place-content-start px-3 py-3 place-items-center">
                         <h2 className="text-lg">Circuits</h2><button id='cir' onClick={handleRemove} className="text-secondary-content btn btn-secondary btn-sm rounded-full m-2 border-transparent text-secondary-content border-0  hover:btn-primary hover:text-primary-content"> Remove All </button>
-                        <ul>
+                        <ul className="place-self-start">
                             {circuit.map((e: any, index: number) => {
                                 return <li key={index}>{e}</li>
                             })}
